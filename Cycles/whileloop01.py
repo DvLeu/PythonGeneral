7c5357fb28ca17610b79f61d53a339f79df6fcd3
Dvleu <L21020400@veracruz.tecnm.mx>
2023-10-07 17:21:01 -0600

    whileloop01.py To whileloop04.py added calculator.py {Interest calculator.py added}
    Signed-off-by: Dvleu <l21020400@veracruz.tecnm.mx>
    Signed-off-by: Dvleu <L21020400@veracruz.tecnm.mx>

---
+++ Cycles/whileloop01.py
@@ -0,0 +1,6 @@
+name = input("Enter your name : ")
+while name == "":
+    print("No name ")
+    name = input("Enter your name : ")
+else :
+    print(f"Hello {name}")
---
+++ Cycles/whileloop03.py
@@ -0,0 +1,5 @@
+food = input("Enter the food you like (q to quit) : ")
+while not food == "q":
+    print(f"You like {food }")
+    food = input("Enter another food you like (q to quit) : ")
+print("Bye")
