49255fa423129f3b8d228622f3decb216bf1cec7
Dvleu <L21020400@veracruz.tecnm.mx>
2023-10-08 14:51:14 -0600

    terniaryOp.py added to main project to have an example of what is and used on future projetcts
    Signed-off-by: Dvleu <L21020400@veracruz.tecnm.mx>

---
+++ IfStatements/terniaryOp.py
@@ -0,0 +1,12 @@
+condicion = 1
+
+# if condicion :
+#     print("Condicion verdadera ")
+# else:
+#     print("Condicion Falsa")
+
+
+#Example only recomended in very short code lines.
+#Only IF and ELSE
+
+print('Condicion verdadera ') if condicion else print('Condicion Falsa ')
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PythonGeneral
 Tutorial for very begginers on python, some will be added later.
+
+
