13dd8a65ae06f6cb880b391a6687dbde07bf6d8d
DvLeu <l21020400@veracruz.tecnm.mx>
2023-10-09 14:53:56 -0600

    Cambios realizados funciones

---
+++ FunctionsOnPy/function01.py
@@ -0,0 +1,14 @@
+
+def greet_user(username):
+    print(f"Hola, {username.title() }! ")
+greet_user('David ')
+
+#Ejercicio 01 
+def queaprendi_01(aprendido):
+    print(f"El dia de hoy aprendi {aprendido.title()}")
+queaprendi_01('Hoy aprendi a hacer funciones en python ')
+    
+#Ejercicio 02
+def favbook(libro):
+    print(f"Mi libro favorito es {libro.title()}!")
+favbook('python crash course ')
---
+++ FunctionsOnPy/pets.py
@@ -0,0 +1,5 @@
+def describe_pet(nombre_mascota, tipo_animal='perro'):
+    #Información de mascota
+    print(f"\n Yo tengo un {tipo_animal} . ")
+    print(f"Mi {tipo_animal} se llama {nombre_mascota}")
+describe_pet(nombre_mascota= 'Escupi')
